6804d671b5e08d31ae93e3770bd9081c96a1cf36
Shawnlaroy <lashawnroyster@gmail.com>
2024-06-29 18:44:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print ('Shawn Roy')
+print ('o----')
+print (' ||||')
+print ('$' * 10)
+print ('Shawn  ' * 10)
+price = 10
+print(price)
+price = 20
+print(price)
+rating = 4.9
+print (rating)
+first_name = 'John'
+last_name = 'Smith'
+age = 20
+status = 'new ' + 'patient'
+print(last_name,first_name)
+print(age)
+print(status)
+name = 'Shawn'
+input('What is your name? ')
+print('Hi '+ name)
+favorite_color = input('What is your favorite color? ')
+print(name + ' likes ' + favorite_color)
+birth_year = input('Birth year: ')
+age = 2024 - int(birth_year)
+print(age)
+weight_lbs = input('Weight (lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg) 
